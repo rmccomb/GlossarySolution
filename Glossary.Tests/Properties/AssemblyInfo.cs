eed8317ee07d36298d13113619cf2fd5293f836b
Robert McComb <mccombenator@gmail.com>
2018-05-16 21:52:23 +1000

    basic tests

---
+++ Glossary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Glossary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Glossary.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a0982db-9093-4ed5-b113-db3b0273adb3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Glossary.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Glossary.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Glossary.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AccessDatabase()
+        {
+            using (var ctx = new GlossaryEntities())
+            {
+                var defs = (from d in ctx.Definitions select d).ToList();
+                defs.ForEach(d => Debug.WriteLine($"{d.DefinitionId} {d.Term} {d.TermDefinition}"));
+            }
+        }
+
+        [TestMethod]
+        public void SaveData()
+        {
+            using (var ctx = new GlossaryEntities())
+            {
+                ctx.Definitions.AddRange(
+                    new Definition[]
+                    {
+                        new Definition { Term = "abyssal plain", TermDefinition = "The ocean floor off the continental margin, usually very flat with a slight slope." },
+                        new Definition { Term="accrete", TermDefinition="To add terranes..." },
+                        new Definition { Term="alkaline", TermDefinition = "Term pertaining to highly basic, as opposed to acidic, substance" }
+                    });
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
